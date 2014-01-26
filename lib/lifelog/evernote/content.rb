0a6d362abc5f0b092b6ca85839158967ba8db950
gong023 <gon.gong.gone@gmail.com>
2014-01-27 02:51:52 +0900

    add validation for erb

--- lib/lifelog/evernote/content.rb
+++ lib/lifelog/evernote/content.rb
@@ -4,8 +4,9 @@ class Lifelog
       require 'erb'
       require 'pathname'
 
+      TEMPLATED = ['tweet'].freeze
+
       def push(name, value)
-        value = [] if value.nil?
         instance_variable_set("@#{name}", value)
       end
 
@@ -16,6 +17,11 @@ class Lifelog
       end
 
       def validate_before_render
+        TEMPLATED.each do |need_templated|
+          in_instance = instance_variable_get("@#{need_templated}")
+          instance_variable_set("@#{need_templated}", ['no-data']) if in_instance.nil? || in_instance.empty?
+        end
+        instance_variables
       end
     end
   end
--- spec/lifelog/evernote/content_spec.rb
+++ spec/lifelog/evernote/content_spec.rb
@@ -12,4 +12,22 @@ describe Lifelog::Evernote::Content do
       expect(subject).to match /<\/en-note>$/
     end
   end
+
+  describe '#validate_before_render' do
+    subject { instance.validate_before_render }
+
+    shared_examples_for 'create all value for erb' do
+      it { expect(subject).to eq [:@tweet] }
+    end
+
+    context 'already set' do
+      before { instance.instance_variable_set("@tweet", ['some tweet']) }
+      it_behaves_like 'create all value for erb'
+      it { subject; expect(instance.instance_eval { @tweet }).to eq ['some tweet']}
+    end
+    context 'no variable' do
+      it_behaves_like 'create all value for erb'
+      it { subject; expect(instance.instance_eval { @tweet }).to eq ['no-data']}
+    end
+  end
 end
