097b5a02622fe6511ce71a52e3dbde2e4368546c
gong023 <gon.gong.gone@gmail.com>
2014-03-06 22:09:49 +0900

    add nullobject spec

---
+++ spec/lifelog/dom/service/nullobject_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Lifelog::Maybe do
+  include Lifelog::Maybe
+
+  describe '#maybe' do
+    subject { maybe(nil) }
+
+    context 'nil' do
+      it { expect(subject).to be_instance_of Lifelog::NullObject }
+      it { expect(subject.to_a).to eq [] }
+      it { expect(subject.to_s).to eq '' }
+      it { expect(subject.to_i).to eq 0 }
+      it { expect(subject.to_f).to eq 0.0 }
+      it { expect(subject[:invalid_key]).to be_instance_of Lifelog::NullObject }
+      it { expect(subject.undefined_method).to be_instance_of Lifelog::NullObject }
+    end
+  end
+end
