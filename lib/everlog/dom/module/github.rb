6ca08edbe579342a4725e9aa54e7aef46c79b53e
gong023 <gon.gong.gone@gmail.com>
2014-06-03 15:32:09 +0900

    push github application layer to domain

--- lib/everlog/dom/module/github.rb
+++ lib/everlog/dom/module/github.rb
@@ -1,28 +1,30 @@
 class Everlog
-  class Dom::Module::Github
+  class Dom::Module::Github < Dom::Module
+    class << self
+      def fetch_since date
+        @config = Dom::Entity::Config.moves
+        repo_names.map { |r| repo_activity(r, date) }.compact
+      end
 
-    def initialize api_config
-      @api_config = api_config
-    end
+      def api_client
+        @api_client ||= Inf::Api::Github.new(@config)
+      end
 
-    def api_client
-      @api_client ||= Inf::Api::Github.new(@api_config)
-    end
+      def repo_names
+        api_client.repos.map { |r| r.full_name }
+      end
 
-    def repo_names
-      api_client.repos.map { |r| r.full_name }
-    end
+      def repo_activity(repo_name, date)
+        repo_commits = commits(repo_name.sub(/.*\//, ''), date)
+        return nil if repo_commits.nil?
+        Dom::Value::Github::Repository_Activity.new(repo_name, repo_commits)
+      end
 
-    def repo_activity(repo_name, date)
-      repo_commits = commits(repo_name.sub(/.*\//, ''), date)
-      return nil if repo_commits.nil?
-      Dom::Value::Github::Repository_Activity.new(repo_name, repo_commits)
-    end
-
-    def commits(repo, date)
-      response = api_client.commits_on(repo, date)
-      return nil if response.empty?
-      response.map { |r| Dom::Value::Github::Commit.new(r) }.reverse
+      def commits(repo, date)
+        response = api_client.commits_on(repo, date)
+        return nil if response.empty?
+        response.map { |r| Dom::Value::Github::Commit.new(r) }.reverse
+      end
     end
   end
 end
